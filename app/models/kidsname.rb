ef916cff4d5c8c7c27d5fb135ed14cc1f044a5a1
ankonym <ankonym@gmail.com>
2015-08-03 11:19:51 +0200

    Fixed gender validation

--- app/models/kidsname.rb
+++ app/models/kidsname.rb
@@ -1,4 +1,5 @@
 class Kidsname < ActiveRecord::Base
   attr_accessible :gender, :name, :rank, :score, :count
   validates :name, uniqueness:true, presence: true
+  validates :gender, presence: true
 end
